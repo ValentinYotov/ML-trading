59338db010cef29c3448a2d201dfb97849b608ed
Valentin <vyotov07@gmail.com>
2025-12-16 11:47:35 +0200

    Add API integration for news retrieval and enhance date handling in MLTrader

--- tradingbot.py
+++ tradingbot.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import os
 from dotenv import load_dotenv
 from alpaca_trade_api import REST
+from timedelta import Timedelta
 load_dotenv()
 
 API_KEY = os.getenv('API_KEY')
@@ -26,6 +27,7 @@ class MLTrader(Strategy):
         self.sleeptime = '24H'
         self.last_trade = None
         self.cash_at_risk = cash_at_risk
+        self.api = REST(base_url=BASE_URL, key_id=API_KEY, secret_key=API_SECRET)
 
 
     def position_sizing(self):
@@ -33,6 +35,15 @@ class MLTrader(Strategy):
         last_price = self.get_last_price(self.symbol)
         quantity = round(cash*self.cash_at_risk/last_price)
         return cash, last_price, quantity
+    
+    def get_dates(self):
+        today = self.get_datetime()
+        three_days_prior = today - Timedelta(days=3)
+        return today.strftime('%Y-%m-%d'), three_days_prior.strftime('%Y-%m-%d' )
+    
+    def get_news(self):
+        today, three_days_prior = self.get_dates()
+        news = self.api.get_news(symbol = self.symbol, start =three_days_prior, end = today)
 
    
 
